6201ad945fe121f36bca499361d8191efb253bd7
ertechin <erselertekin@hotmail.com>
2024-06-19 19:07:35 +0300

    add svg for edit/show page button, refactor user index page

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,13 @@
 module ApplicationHelper
+  def embedded_svg(filename)
+    assets = Rails.application.assets
+    asset = assets.find_asset("#{filename}.svg")
+    if asset
+      file = asset.source.force_encoding('UTF-8')
+      doc = Nokogiri::HTML::DocumentFragment.parse file
+    else
+      doc = "<!-- SVG #{filename} not found -->"
+    end
+    raw doc
+  end
 end
